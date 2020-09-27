815c28aca39aaf2e17a5b010a3e42330edd4102a
CJ <salinecjr@gmail.com>
2020-09-27 12:52:55 -0700

    Add larger page and extended note range

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ pygame.init()
 
 # Set canvas size, background refresh color, note speed
 # Speed - shows a note every n milliseconds
-size = width, height = 400, 200
+size = width, height = 800, 600
 white = 255, 255, 255
 speed = 1000
 
 screen = pygame.display.set_mode(size)
+
+chirp = pygame.mixer.Sound('snd/1-000.ogg')
 
 # Container of Note objects
 list_of_qNotes = note_maker.make_Notes()
@@ -22,49 +24,126 @@ list_of_qNotes = note_maker.make_Notes()
 list_of_staff_notes = []
 list_of_staff_notes_rect = []
 
-# x position of first note
-x_note_start = 75
+# y of mid line in top row
+initial_score_y_offset = 133
+# x position of first note in top row
+x_note_start_first_line = 150
 # x distance between notes
-x_increment = 30
+x_increment_first_line = 40
 # counter used for incrementing x distance
 m = 0
 
-# Fill note and rect lists with random notes to be displayed on the staff.
-for n in range(len(list_of_qNotes)):
+for n in range(16):
     random_int = random.randint(0, len(list_of_qNotes) - 1)
 
     note_obj = pygame.image.load(
         list_of_qNotes[random_int].image).convert_alpha()
     note_rect = note_obj.get_rect()
-    note_rect.y = list_of_qNotes[random_int].staff_pos
-    note_rect.x = x_note_start + x_increment * m
+
+    note_rect.y = list_of_qNotes[random_int].staff_pos + initial_score_y_offset
+    note_rect.x = x_note_start_first_line + x_increment_first_line * m
 
     list_of_staff_notes.append(note_obj)
     list_of_staff_notes_rect.append(note_rect)
 
     m = m + 1
 
+# mod 20
+# y of mid line in top row
+score_subsequent_y_offset = 80
+# x position of first note in top row
+x_note_start = 27
+# x distance between notes
+x_increment = 38
+# counter used for incrementing x distance
+for n in range(100):
+    random_int = random.randint(0, len(list_of_qNotes) - 1)
+
+    y = 1 + n // 20
+    m = n % 20
+    note_obj = pygame.image.load(list_of_qNotes[random_int %
+                                                16].image).convert_alpha()
+    note_rect = note_obj.get_rect()
+
+    note_rect.y = list_of_qNotes[random_int %
+                                 16].staff_pos + initial_score_y_offset + (
+                                     score_subsequent_y_offset * y)
+    note_rect.x = x_note_start + x_increment * m
+
+    list_of_staff_notes.append(note_obj)
+    list_of_staff_notes_rect.append(note_rect)
+
 # Make note
-staff = pygame.image.load("art/staff.gif")
+# Each staff is 80 px from the last. First starts at 115
+staff = pygame.image.load("art/largescore.gif")
 staff_rect = staff.get_rect()
 
 # Set time variables for note appearance tempo
 init_time = pygame.time.get_ticks()
 time_increment = 1500
+timer = init_time
+frame = 0
+
+clock = pygame.time.Clock()
+te = 0
+screen.blit(staff, staff_rect)
+screen.blit(list_of_staff_notes[0], list_of_staff_notes_rect[0])
+screen.blit(list_of_staff_notes[1], list_of_staff_notes_rect[1])
+
+my_event = pygame.USEREVENT + 1
+pygame.time.set_timer(my_event, 1000)
+
+pygame.event.set_allowed(pygame.QUIT)
+pygame.event.set_allowed(pygame.ACTIVEEVENT)
+pygame.event.set_allowed(pygame.KEYDOWN)
+pygame.event.set_allowed(pygame.USEREVENT)
+pygame.event.set_blocked(pygame.MOUSEMOTION)
 
 # Main loop reveals notes at fixed interval
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
 
-    # Clear image, add empty staff, add starting notes
-    screen.fill(white)
-    screen.blit(staff, staff_rect)
-    screen.blit(list_of_staff_notes[0], list_of_staff_notes_rect[0])
+    event = pygame.event.wait()
+    print(event)
 
-    # Every 1000 ms, print a new note
-    current_time = pygame.time.get_ticks()
-    for n in range(int(current_time / speed)):
-        screen.blit(list_of_staff_notes[n], list_of_staff_notes_rect[n])
+    # Schedule the next event
+    if event.type == my_event:
+        chirp.play()
+        print(pygame.time.get_ticks() - timer)
+        timer = pygame.time.get_ticks()
 
-    pygame.display.flip()
+        #for n in range(116):
+        #    screen.blit(list_of_staff_notes[n], list_of_staff_notes_rect[n])
+
+        screen.blit(list_of_staff_notes[frame],
+                    list_of_staff_notes_rect[frame])
+
+        frame += 1
+        pygame.display.flip()
+
+    elif event.type == pygame.KEYDOWN:
+        sys.exit()
+    elif event.type == pygame.QUIT:
+        sys.exit()
+
+    # dt = clock.tick(8)
+    # te += dt
+    # print(te)
+
+    # if te > speed:
+
+    #     # Clear image, add empty staff, add starting notes
+    #     #screen.fill(white)
+    #     chirp.play()
+
+    #     #for n in range(116):
+    #     #    screen.blit(list_of_staff_notes[n], list_of_staff_notes_rect[n])
+    #     screen.blit(list_of_staff_notes[0], list_of_staff_notes_rect[0])
+    #     screen.blit(list_of_staff_notes[1], list_of_staff_notes_rect[1])
+
+    #     screen.blit(list_of_staff_notes[frame],
+    #                 list_of_staff_notes_rect[frame])
+    #     frame += 1
+
+    #     #for n in range(int(pygame.time.get_ticks() / speed)):
+    #     #    screen.blit(list_of_staff_notes[n], list_of_staff_notes_rect[n])
+    #     te = 0
--- note_maker.py
+++ note_maker.py
@@ -18,41 +18,77 @@ class Note():
         self.note_value = note_value
 
 
+# position of notes is relative to middle bar of staff
 def make_Notes():
-
     list_of_notes = []
 
-    qnote_d4 = Note("QD4", "art/qnote-stemtop.gif", 37, "quarter")
+    # Prev 52
+    qnote_b3 = Note("QA3", "art/qnote-onebar-stemtop.gif", 5, "quarter")
+    list_of_notes.append(qnote_b3)
+
+    # Prev 47
+    qnote_c4 = Note("QB3", "art/qnote-midbar-stemtop.gif", 0, "quarter")
+    list_of_notes.append(qnote_c4)
+
+    # Prev 42
+    #qnote_c4 = Note("QC4", "art/qnote-stemtop.gif", 0, "quarter")
+    #list_of_notes.append(qnote_c4)
+
+    # Prev 37
+    qnote_d4 = Note("QD4", "art/qnote-stemtop.gif", -5, "quarter")
     list_of_notes.append(qnote_d4)
 
-    qnote_e4 = Note("QE4", "art/qnote-stemtop.gif", 32, "quarter")
+    # Prev 32
+    qnote_e4 = Note("QE4", "art/qnote-stemtop.gif", -10, "quarter")
     list_of_notes.append(qnote_e4)
 
-    qnote_f4 = Note("QF4", "art/qnote-stemtop.gif", 27, "quarter")
+    # Prev 27
+    qnote_f4 = Note("QF4", "art/qnote-stemtop.gif", -15, "quarter")
     list_of_notes.append(qnote_f4)
 
-    qnote_g4 = Note("QG4", "art/qnote-stemtop.gif", 21, "quarter")
+    # Prev 21
+    qnote_g4 = Note("QG4", "art/qnote-stemtop.gif", -21, "quarter")
     list_of_notes.append(qnote_g4)
 
-    qnote_a4 = Note("QA4", "art/qnote-stemtop.gif", 15, "quarter")
+    # Prev 15
+    qnote_a4 = Note("QA4", "art/qnote-stemtop.gif", -27, "quarter")
     list_of_notes.append(qnote_a4)
 
-    qnote_b4 = Note("QB4", "art/qnote-stembottom.gif", 42, "quarter")
+    # Prev 42
+    qnote_b4 = Note("QB4", "art/qnote-stembottom.gif", 0, "quarter")
     list_of_notes.append(qnote_b4)
 
-    qnote_c5 = Note("QC5", "art/qnote-stembottom.gif", 37, "quarter")
+    # Prev 37
+    qnote_c5 = Note("QC5", "art/qnote-stembottom.gif", -6, "quarter")
     list_of_notes.append(qnote_c5)
 
-    qnote_d5 = Note("QD5", "art/qnote-stembottom.gif", 31, "quarter")
+    # Prev 31
+    qnote_d5 = Note("QD5", "art/qnote-stembottom.gif", -11, "quarter")
     list_of_notes.append(qnote_d5)
 
-    qnote_e5 = Note("QE5", "art/qnote-stembottom.gif", 25, "quarter")
+    # Prev 25
+    qnote_e5 = Note("QE5", "art/qnote-stembottom.gif", -17, "quarter")
     list_of_notes.append(qnote_e5)
 
-    qnote_f5 = Note("QF5", "art/qnote-stembottom.gif", 20, "quarter")
+    # Prev 20
+    qnote_f5 = Note("QF5", "art/qnote-stembottom.gif", -22, "quarter")
     list_of_notes.append(qnote_f5)
 
-    qnote_g5 = Note("QG5", "art/qnote-stembottom.gif", 15, "quarter")
+    # Prev 15
+    qnote_g5 = Note("QG5", "art/qnote-stembottom.gif", -27, "quarter")
     list_of_notes.append(qnote_g5)
+
+    # Prev 5
+    qnote_a5 = Note("QB5", "art/qnote-midbar-stembottom.gif", -32, "quarter")
+    list_of_notes.append(qnote_a5)
+
+    # Prev 0
+    qnote_b5 = Note("QC6", "art/qnote-onebar-stembottom.gif", -38, "quarter")
+    list_of_notes.append(qnote_b5)
+
+    # Prev 10
+    qnote_c6 = Note("QA5", "art/qnote-twomidbar-stembottom.gif", -46,
+                    "quarter")
+    list_of_notes.append(qnote_c6)
 
     return list_of_notes
